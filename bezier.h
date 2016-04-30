0e99899a12691f1fa8508eac49bc7cff3892fd0c
italankin <italankin@gmail.com>
2016-04-30 15:55:50 +0500

    Replace tabs

--- bezier.h
+++ bezier.h
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-const int DIMEN_BUFFER = 256 + 2;					// max divisions
+const int DIMEN_BUFFER = 256 + 2;               // max divisions
 
-enum OBJECT_TYPE {									// bezier object types
-	SURFACE1D = 1,
-	SURFACE2D
+enum OBJECT_TYPE {                              // bezier object types
+    SURFACE1D = 1,
+    SURFACE2D
 };
 
 // basic class Point
@@ -18,31 +18,31 @@ class Point {
 
 public:
 
-	float x, y, z;
+    float x, y, z;
 
-	Point(float* d) {
-		x = d[0];
-		y = d[1];
-		z = d[2];
-	}
+    Point(float* d) {
+        x = d[0];
+        y = d[1];
+        z = d[2];
+    }
 
-	Point(float x0 = 0.0, float y0 = 0.0, float z0 = 0.0) {
-		x = x0;
-		y = y0;
-		z = z0;
-	}
+    Point(float x0 = 0.0, float y0 = 0.0, float z0 = 0.0) {
+        x = x0;
+        y = y0;
+        z = z0;
+    }
 
-	Point operator*(float s) {
-		return Point(x * s, y * s, z * s);
-	}
+    Point operator*(float s) {
+        return Point(x * s, y * s, z * s);
+    }
 
-	Point operator*(float s) const {
-		return Point(x * s, y * s, z * s);
-	}
+    Point operator*(float s) const {
+        return Point(x * s, y * s, z * s);
+    }
 
-	Point operator+(Point p2) {
-		return Point(x + p2.x, y + p2.y, z + p2.z);
-	}
+    Point operator+(Point p2) {
+        return Point(x + p2.x, y + p2.y, z + p2.z);
+    }
 
 };
 
@@ -50,8 +50,8 @@ public:
 class surface {
 
 public:
-	int dimen_n;
-	int type;
+    int dimen_n;
+    int type;
 
 };
 
@@ -60,17 +60,17 @@ class surface1d: public surface {
 
 public:
 
-	Point *anchors[DIMEN_BUFFER];
+    Point *anchors[DIMEN_BUFFER];
 
-	surface1d() {
-		dimen_n = 0;
-		type = SURFACE1D;
-	}
+    surface1d() {
+        dimen_n = 0;
+        type = SURFACE1D;
+    }
 
-	surface1d(int n) {
-		dimen_n = (n > DIMEN_BUFFER) ? DIMEN_BUFFER : n;
-		type = SURFACE1D;
-	}
+    surface1d(int n) {
+        dimen_n = (n > DIMEN_BUFFER) ? DIMEN_BUFFER : n;
+        type = SURFACE1D;
+    }
 
 };
 
@@ -79,92 +79,92 @@ class surface2d: public surface {
 
 public:
 
-	int dimen_m;
+    int dimen_m;
 
-	Point *anchors[DIMEN_BUFFER][DIMEN_BUFFER];
+    Point *anchors[DIMEN_BUFFER][DIMEN_BUFFER];
 
-	surface2d() {
-		dimen_n = dimen_m = 0;
-		type = SURFACE2D;
-	}
+    surface2d() {
+        dimen_n = dimen_m = 0;
+        type = SURFACE2D;
+    }
 
-	// by square array
-	surface2d(int s) {
-		dimen_m = dimen_n = (s > DIMEN_BUFFER) ? DIMEN_BUFFER : s;
-		type = SURFACE2D;
-	}
+    // by square array
+    surface2d(int s) {
+        dimen_m = dimen_n = (s > DIMEN_BUFFER) ? DIMEN_BUFFER : s;
+        type = SURFACE2D;
+    }
 
-	// by non-square array
-	surface2d(int n, int m) {
-		dimen_n = (n > DIMEN_BUFFER) ? DIMEN_BUFFER : n;
-		dimen_m = (m > DIMEN_BUFFER) ? DIMEN_BUFFER : m;
-		type = SURFACE2D;
-	}
+    // by non-square array
+    surface2d(int n, int m) {
+        dimen_n = (n > DIMEN_BUFFER) ? DIMEN_BUFFER : n;
+        dimen_m = (m > DIMEN_BUFFER) ? DIMEN_BUFFER : m;
+        type = SURFACE2D;
+    }
 
 };
 
 // factorial
 int fact(int n) {
-	return (n <= 1) ? 1 : n * fact(n - 1);
+    return (n <= 1) ? 1 : n * fact(n - 1);
 }
 
 // bernstein polynomial function
 float bernstein(int n, int i, float u) {
-	return fact(n) / (fact(i) * fact(n - i)) * pow(u, i) * pow(1 - u, n - i);
+    return fact(n) / (fact(i) * fact(n - i)) * pow(u, i) * pow(1 - u, n - i);
 }
 
 //
 surface1d getBezier(surface1d a, int divs) {
-	surface1d s(divs + 1);
+    surface1d s(divs + 1);
 
-	int t, i;
-	float tv, b;
+    int t, i;
+    float tv, b;
 
-	Point *p;
+    Point *p;
 
-	for (t = 0; t <= divs; t++) {
-		tv = ((float) t) / ((float) divs);
-		p = new Point();
-		for (i = 0; i <= a.dimen_n; i++) {
-			b = bernstein(a.dimen_n, i, tv);
-			*p = *p + *a.anchors[i] * b;
-		} // END i
-		s.anchors[t] = p;
-	} // END t
+    for (t = 0; t <= divs; t++) {
+        tv = ((float) t) / ((float) divs);
+        p = new Point();
+        for (i = 0; i <= a.dimen_n; i++) {
+            b = bernstein(a.dimen_n, i, tv);
+            *p = *p + *a.anchors[i] * b;
+        } // END i
+        s.anchors[t] = p;
+    } // END t
 
-	return s;
+    return s;
 }
 
 //
 surface2d getBezier(surface2d a, int divs) {
-	surface2d s(divs + 1);
+    surface2d s(divs + 1);
 
-	int i, j, u, v;
-	float ur, vr, bi, bj;
+    int i, j, u, v;
+    float ur, vr, bi, bj;
 
-	Point *p;
+    Point *p;
 
-	glColor3f(1.0, 0.0, 1.0);
+    glColor3f(1.0, 0.0, 1.0);
 
-	for (u = 0; u <= divs; u++) {
-		ur = ((float) u) / ((float) divs);
-		for (v = 0; v <= divs; v++) {
-			vr = ((float) v) / ((float) divs);
-			p = new Point();
-			for (i = 0; i <= a.dimen_n; i++) {
-				bi = bernstein(a.dimen_n, i, ur);
-				for (j = 0; j <= a.dimen_m; j++) {
-					bj = bernstein(a.dimen_m, j, vr);
-//					cout << i << " " << j << endl;
-//					cout << a.anchors[i][j]->x << "," << a.anchors[i][j]->y << "," << a.anchors[i][j]->z << " \n";
-					*p = *p + *a.anchors[i][j] * bi * bj;
-				} // END j
-			} // END i
-			s.anchors[u][v] = p;
-		} // END v
-	} // END u
+    for (u = 0; u <= divs; u++) {
+        ur = ((float) u) / ((float) divs);
+        for (v = 0; v <= divs; v++) {
+            vr = ((float) v) / ((float) divs);
+            p = new Point();
+            for (i = 0; i <= a.dimen_n; i++) {
+                bi = bernstein(a.dimen_n, i, ur);
+                for (j = 0; j <= a.dimen_m; j++) {
+                    bj = bernstein(a.dimen_m, j, vr);
+//                    cout << i << " " << j << endl;
+//                    cout << a.anchors[i][j]->x << "," << a.anchors[i][j]->y << "," << a.anchors[i][j]->z << " \n";
+                    *p = *p + *a.anchors[i][j] * bi * bj;
+                } // END j
+            } // END i
+            s.anchors[u][v] = p;
+        } // END v
+    } // END u
 
-	return s;
+    return s;
 
 }
 
--- utils.h
+++ utils.h
@@ -4,48 +4,48 @@
 class Mouse {
 
 public:
-	int x;											// current mouse x
-	int y;											// current mouse y
-	int last_x;										// previous mouse position x
-	int last_y;										// previous mouse position y
+    int x;                                      // current mouse x
+    int y;                                      // current mouse y
+    int last_x;                                 // previous mouse position x
+    int last_y;                                 // previous mouse position y
 
-	int button;
-	int state;
+    int button;
+    int state;
 
-	bool active;
+    bool active;
 
-	Mouse() {
-		x = 0;
-		y = 0;
-		last_x = 0;
-		last_y = 0;
-		button = 0;
-		state = 0;
-		active = false;
-	}
+    Mouse() {
+        x = 0;
+        y = 0;
+        last_x = 0;
+        last_y = 0;
+        button = 0;
+        state = 0;
+        active = false;
+    }
 
-	void set(int new_x, int new_y) {
-		last_x = x;
-		last_y = y;
-		x = new_x;
-		y = new_y;
-	}
+    void set(int new_x, int new_y) {
+        last_x = x;
+        last_y = y;
+        x = new_x;
+        y = new_y;
+    }
 
-	int dx() {
-		return x - last_x;
-	}
+    int dx() {
+        return x - last_x;
+    }
 
-	int dy() {
-		return y - last_y;
-	}
+    int dy() {
+        return y - last_y;
+    }
 
 };
 
 // convert int to string type
 string int_to_string(int i) {
-	stringstream ss;
-	ss << i;
-	return ss.str();
+    stringstream ss;
+    ss << i;
+    return ss.str();
 }
 
 #endif /* MOUSE_H_ */
